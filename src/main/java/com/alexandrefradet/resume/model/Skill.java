3e26725fe4a8eeec9cbfbf831d62cff3ce3c947f
afradet <alex.fradet.1@gmail.com>
2018-03-26 18:00:13 +0200

    Add collection

--- src/main/java/com/alexandrefradet/resume/model/Skill.java
+++ src/main/java/com/alexandrefradet/resume/model/Skill.java
@@ -1,27 +1,25 @@
 package com.alexandrefradet.resume.model;
 
-import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OrderColumn;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
-public class Post {
+@Setter
+public class Skill {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
-	private LocalDate createdAt;
-	
-	private LocalDate modifiedAt;
-	
-	private String title;
-	
-	private String content;
+	@ElementCollection
+	@OrderColumn(name = "details_order")
+	List<String> details;
 }
